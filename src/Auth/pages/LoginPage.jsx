5b4bbf8ba0380f77f1976c4291d56b8076f28374
guidomora <guidomorabito161@hotmail.com>
2023-01-14 13:29:17 -0300

    3rd commit

--- src/Auth/pages/LoginPage.jsx
+++ src/Auth/pages/LoginPage.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
+  const navigate = useNavigate()
+
+  const onLogin = () => {
+    // Una vez que pasa el login te manda a la pag principal
+    navigate("/", {replace: true})
+  }
+
+
   return (
-    <h1>Login</h1>
+    <div>
+      <h1>Login</h1>
+      <hr />
+      <button className='btn btn-primary' onClick={onLogin}>Login</button>
+    </div>
   )
 }
 
--- src/Ui/NavBar.jsx
+++ src/Ui/NavBar.jsx
@@ -1,8 +1,17 @@
 
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+    const navigate = useNavigate()
+
+    const onLogout = () => {
+        // primer argumento establece la ruta a la cual vamos a navegar, el segundo evita que la persona pueda
+        // volver para atras en el historial (en este caso pq seria un login, nos sirve)
+        navigate("/login", {replace: true})
+    }
+
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             <Link
@@ -37,7 +46,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>Guido</span>
-                    <button className='nav-item nav-link btn'>Logout</button>
+                    <button className='nav-item nav-link btn' onClick={onLogout}>Logout</button>
                 </ul>
             </div>
         </nav>
