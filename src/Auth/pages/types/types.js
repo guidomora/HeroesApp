ca77b70603e33711f8b481c516b0b0a0dbb7cd45
guidomora <guidomorabito161@hotmail.com>
2023-01-15 20:41:46 -0300

    routes

---
+++ src/Auth/pages/types/types.js
@@ -0,0 +1,7 @@
+// Forma centralizada con los nombres de as acciones que voy a disparar
+// con redux esto se elimina
+
+export const types = {
+    login: "[Auth] Login",
+    logout: "[Auth] Logout",
+}
