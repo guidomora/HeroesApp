951a06a6e265b5bd9cdb00c0c405e370820c3982
guidomora <guidomorabito161@hotmail.com>
2023-01-17 23:18:05 -0300

    finished

--- src/Heroes/Components/Herocard.jsx
+++ src/Heroes/Components/Herocard.jsx
@@ -11,7 +11,7 @@ const Herocard = ({
   characters,
 }) => {
 
-    const heroImageUrl = `/Assets/heroes/${id}.jpg`
+    const heroImageUrl = `/heroes/${id}.jpg`
 
   return (
     // Animaciones clase de animate etc
